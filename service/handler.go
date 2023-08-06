40e99720a1a18eab8b7cc31784dd881a35907fde
Vaala Cat <me@vaala.cat>
2023-08-06 20:29:22 +0800

    feat: add opitons handler

--- service/handler.go
+++ service/handler.go
@@ -14,6 +14,17 @@ func Run() {
 	{
 		v1.GET("/models", models.GetModelsEndpoint)
 		v1.POST("/chat/completions", chat.ChatEndpoint)
+		v1.OPTIONS("/chat/completions", OptionsHandler)
 	}
 	r.Run(":8080")
+}
+
+func OptionsHandler(c *gin.Context) {
+	// Set headers for CORS
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "POST")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
